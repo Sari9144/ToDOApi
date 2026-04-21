dce2c0ed20b06ab5324f1ba8ca8aed569a790d57
sari <sari0504109144@gmail.com>
2026-04-21 17:11:09 +0300

    Fix: Added MySQL configuration and retry logic in Program.cs

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -3,17 +3,15 @@ using TodoApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// --- עדכון הגדרת ה-Database ---
-// שליפת מחרוזת החיבור ממשתני הסביבה של Render
+// 1. שליפת מחרוזת החיבור
 var connectionString = builder.Configuration.GetConnectionString("ToDoDb");
 
-// הגדרת גרסה קבועה כדי למנוע שגיאות חיבור בעלייה (AutoDetect)
+// 2. הגדרת גרסה קבועה (במקום AutoDetect) כדי למנוע קריסה בעלייה
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 33)); 
 
 builder.Services.AddDbContext<ToDoDbContext>(options =>
     options.UseMySql(connectionString, serverVersion, 
         mysqlOptions => mysqlOptions.EnableRetryOnFailure()));
-// ------------------------------
 
 // הגדרת CORS
 builder.Services.AddCors(options => {
@@ -24,34 +22,27 @@ builder.Services.AddCors(options => {
     });
 });
 
-// הגדרת Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// --- קוד ליצירת טבלאות אוטומטית ---
+// 3. יצירת הטבלאות באופן אוטומטי אם הן חסרות
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ToDoDbContext>();
     db.Database.EnsureCreated(); 
 }
-// ---------------------------------
 
-// הפעלת Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseCors("AllowAll"); 
 
-// Routes
 app.MapGet("/", () => "ToDo API is running!");
 
-// שליפת כל המשימות
 app.MapGet("/items", async (ToDoDbContext db) => 
     await db.Items.ToListAsync());
 
-// הוספת משימה חדשה
 app.MapPost("/items", async (ToDoDbContext db, Item newItem) => {
     db.Items.Add(newItem);
     await db.SaveChangesAsync();
@@ -69,7 +60,6 @@ app.MapPut("/items/{id}", async (ToDoDbContext db, int id, Item inputItem) => {
     return Results.NoContent();
 });
 
-// מחיקת משימה
 app.MapDelete("/items/{id}", async (ToDoDbContext db, int id) => {
     if (await db.Items.FindAsync(id) is Item item) {
         db.Items.Remove(item);
